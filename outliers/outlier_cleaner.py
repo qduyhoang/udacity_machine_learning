82bc8563659c08ba6dd1f234dac786db2b05b534
Duy <qduyhoang@gmail.com>
2018-12-07 14:59:27 -0500

    Complete outlierCleaner() function

--- outliers/outlier_cleaner.py
+++ outliers/outlier_cleaner.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 
 
 def outlierCleaner(predictions, ages, net_worths):
@@ -10,11 +10,26 @@ def outlierCleaner(predictions, ages, net_worths):
         Return a list of tuples named cleaned_data where 
         each tuple is of the form (age, net_worth, error).
     """
-    
-    cleaned_data = []
 
     ### your code goes here
+    import math
+    cleaned_data = []
 
+
+    sorted_error = []
+    for true_y, predicted_y in zip( net_worths, predictions ):
+        sorted_error.append( abs( true_y - predicted_y ) )
+    sorted_error.sort()
+
+    #number of training data to be kept
+    num_data_kept = int( math.floor( (len(sorted_error) * 0.9) ) )
+    highest_error = sorted_error[num_data_kept:]
+
+
+    for i in range(len(predictions)):
+        err = abs( net_worths[i] - predictions[i] )
+        if err not in highest_error:
+            cleaned_data.append( ( ages[i], net_worths[i], err ) )
     
     return cleaned_data
 
